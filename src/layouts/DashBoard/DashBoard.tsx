98dc8d7d4a5fbe8a5707e2c3cb3e6514fe530272
Nanda-16 <nandanakrishna1601@gmail.com>
2023-05-03 18:51:34 +0530

    datatbale in designation

--- src/layouts/DashBoard/DashBoard.tsx
+++ src/layouts/DashBoard/DashBoard.tsx
@@ -1,36 +1,19 @@
-// import {
-//   AppBar,
-//   Badge,
-//   Box,
-//   IconButton,
-//   Toolbar,
-//   Typography,
-// } from "@mui/material";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
 
 const DashBoard = () => {
   return (
     <>
-      <Navbar
-        expand="lg"
-        sticky="top"
-        className="border-bottom"
-        style={{ background: "#373c40" }}
-      >
-        <Container fluid>
-          <Navbar.Brand className="text-white me-4">
-            Admin Template
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="my-2 mx-5 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
+      <Box sx={{ flexGrow: 1, paddingBottom: "20px" }}>
+        <AppBar sx={{ backgroundColor: "#373c40" }}>
+          <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
+            <Typography variant="h6">Admin Template</Typography>
+            <div style={{ display: "flex" }}>
               <NavLink
                 to="/dashboard/home"
                 className={({ isActive }) =>
@@ -55,17 +38,15 @@ const DashBoard = () => {
               >
                 <span className="mx-2">Designations</span>
               </NavLink>
-            </Nav>
-            <Navbar.Text className="text-secondary ms-5">
-              <div className="d-flex">
-                <span>Welcome Muhammed Shafi P</span>
-                <div className="ms-2" style={{ borderRadius: "50%" }}></div>
-              </div>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <main className="p-10">
+            </div>
+            <div className="d-flex">
+              <span>Welcome Muhammed Shafi P</span>
+              <div className="ms-2" style={{ borderRadius: "50%" }}></div>
+            </div>
+          </Toolbar>
+        </AppBar>{" "}
+      </Box>
+      <main className="p-5">
         <Outlet />
       </main>
     </>
--- src/pages/Designations/Designations.tsx
+++ src/pages/Designations/Designations.tsx
@@ -1,8 +1,129 @@
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
 
-function Designations() {
+const renderDeleteButton = () => {
   return (
-    <>Designations</>
-  )
-}
+    <IconButton aria-label="delete">
+      <DeleteIcon />
+    </IconButton>
+  );
+};
+const columns: GridColDef[] = [
+  { field: "id", headerName: "SI.No", minWidth: 200 },
+  { field: "name", headerName: "Name", minWidth: 200 },
+  { field: "designation", headerName: "Designation", minWidth: 200 },
+  {
+    field: "action",
+    headerName: "Action",
+    renderCell: renderDeleteButton,
+    minWidth: 200,
+  },
+];
 
-export default Designations
+const rows = [
+  {
+    id: 1,
+    lastName: "Snow",
+    firstName: "Jon",
+    name: "Snow Jon",
+    designation: "Laravel Developer",
+    age: 35,
+  },
+  {
+    id: 2,
+    lastName: "Lannister",
+    firstName: "Cersei",
+    name: "Lannister Cersi",
+    designation: "React Developer",
+    age: 42,
+  },
+  {
+    id: 3,
+    lastName: "Lannister",
+    firstName: "Jaime",
+    name: "Lannister Jaime",
+    designation: "Front End Developer",
+    age: 45,
+  },
+  {
+    id: 4,
+    lastName: "Stark",
+    firstName: "Arya",
+    name: "Stark Arya",
+    designation: "Backend Developer",
+    age: 16,
+  },
+  {
+    id: 5,
+    lastName: "Targaryen",
+    firstName: "Daenerys",
+    name: "Targaryen Daenerys",
+    designation: "Laravel Developer",
+    age: null,
+  },
+  {
+    id: 6,
+    lastName: "Melisandre",
+    firstName: null,
+    name: "Melisandre ",
+    designation: "Laravel Developer",
+    age: 150,
+  },
+  {
+    id: 7,
+    lastName: "Clifford",
+    firstName: "Ferrara",
+    name: "Clifford Ferrara",
+    designation: "Tester",
+    age: 44,
+  },
+  {
+    id: 8,
+    lastName: "Frances",
+    firstName: "Rossini",
+    name: "Frances Rossini",
+    designation: "UI Developer",
+    age: 36,
+  },
+  {
+    id: 9,
+    lastName: "Roxie",
+    firstName: "Harvey",
+    name: "Roxie Harvey",
+    designation: "UX Developer",
+    age: 65,
+  },
+];
+
+export default function Designations() {
+  return (
+    <>
+      <div>
+        <div className="d-flex justify-content-between w-100 mb-3 mt-5">
+          <h3>Designation List</h3>
+          <div>
+            <Button variant="outlined" size="small" color="inherit">
+              <AddSharpIcon /> Add New Record
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="" style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+        />
+      </div>
+    </>
+  );
+}
